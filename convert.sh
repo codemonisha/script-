9a28610fa65cc12ea400ec2b79244f0bf600c613
codemonisha <134149320+codemonisha@users.noreply.github.com>
2023-06-13 17:03:39 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ls /var/lib/jenkins/workspace/job6 | grep -i .html > output
+ls /var/lib/jenkins/workspace/job6 | grep -i .txt > output
 while read line
 do
         n=`echo $line | awk -F "." '{print $1}'`
-        mv $line $n.txt
+        mv $line $n.html
 done < output
 ls
